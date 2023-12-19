e1ffd5677b5ef9b57cdb569979ec17b941e24e9b
nosrednarecneps <85041163+nosrednarecneps@users.noreply.github.com>
2023-12-19 11:57:26 -0700

    updated code to work properly

--- drawbot.ino
+++ drawbot.ino
@@ -29,163 +29,117 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   rotateMotorByAngle(1, 180);
-  delay(1500);
+  delay(500);
 }
 
-void changeMotorPositionOne() {
-  step_number_1++;
-  if(step_number_1 > 3) {
-    step_number_1 = 0;
+void changeMotorPositionOne(bool dir) {
+  if (dir == true) {
+    step_number_1++;
+    if (step_number_1 > 3) {
+      step_number_1 = 0;
+    }
+  } else if (dir == false) {
+    step_number_1--;
+    if (step_number_1 < 0) {
+      step_number_1 = 3;
+    }
   }
 }
 
-void changeMotorPositionTwo() {
-  step_number_2++;
-  if(step_number_2 > 3) {
-    step_number_2 = 0;
+void changeMotorPositionTwo(bool dir) {
+  if (dir == true) {
+    step_number_2++;
+    if (step_number_2 > 3) {
+      step_number_2 = 0;
+    }
+  } else if (dir == false) {
+    step_number_2--;
+    if (step_number_2 < 0) {
+      step_number_2 = 3;
+    }
   }
 }
 
 void rotateMotorByAngle(int motorGroup, int angle) {
   int steps;
-  if (motorGroup == 1 || motorGroup == 3) {
-    steps = map(angle, 0, 360, 0, STEPS_PER_REVOLUTION);
-    for (int i = 0; i < steps; i++) {
-      moveForward(motorGroup);
-      delay(2); // Adjust the delay as needed
-    }
-  } else if (motorGroup == 2) {
-    // Adjust this condition if motorGroup 2 has a different mapping
-    steps = map(angle, 0, 360, 0, STEPS_PER_REVOLUTION);
-    for (int i = 0; i < steps; i++) {
-      moveForward(motorGroup);
-      delay(2); // Adjust the delay as needed
-    }
+  steps = map(angle, 0, 360, 0, STEPS_PER_REVOLUTION);
+  for (int i = 0; i < steps; i++) {
+    moveForward(motorGroup);
+    delay(2); // Adjust the delay as needed
+  } 
+}
+
+void moveLeftMotor(bool dir) {
+  if (step_number_1 == 0) {
+    digitalWrite(SP1, HIGH);
+    digitalWrite(SP2, LOW);
+    digitalWrite(SP3, LOW);
+    digitalWrite(SP4, LOW);
+    changeMotorPositionOne(dir);
+  } else if (step_number_1 == 1) {
+    digitalWrite(SP1, LOW);
+    digitalWrite(SP2, HIGH);
+    digitalWrite(SP3, LOW);
+    digitalWrite(SP4, LOW);
+    changeMotorPositionOne(dir);
+  } else if (step_number_1 == 2) {
+    digitalWrite(SP1, LOW);
+    digitalWrite(SP2, LOW);
+    digitalWrite(SP3, HIGH);
+    digitalWrite(SP4, LOW);
+    changeMotorPositionOne(dir);
+  } else if (step_number_1 == 3) {
+    digitalWrite(SP1, LOW);
+    digitalWrite(SP2, LOW);
+    digitalWrite(SP3, LOW);
+    digitalWrite(SP4, HIGH);
+    changeMotorPositionOne(dir);
   }
 }
 
-void moveForward(int motorGroup){
+void moveRightMotor(bool dir) {
+  if (step_number_2 == 0) {
+    digitalWrite(SP5, HIGH);
+    digitalWrite(SP6, LOW);
+    digitalWrite(SP7, LOW);
+    digitalWrite(SP8, LOW);
+    changeMotorPositionTwo(dir);
+  } else if (step_number_2 == 1) {
+    digitalWrite(SP5, LOW);
+    digitalWrite(SP6, HIGH);
+    digitalWrite(SP7, LOW);
+    digitalWrite(SP8, LOW);
+    changeMotorPositionTwo(dir);
+  } else if (step_number_2 == 2) {
+    digitalWrite(SP5, LOW);
+    digitalWrite(SP6, LOW);
+    digitalWrite(SP7, HIGH);
+    digitalWrite(SP8, LOW);
+    changeMotorPositionTwo(dir);
+  } else if (step_number_2 == 3) {
+    digitalWrite(SP5, LOW);
+    digitalWrite(SP6, LOW);
+    digitalWrite(SP7, LOW);
+    digitalWrite(SP8, HIGH);
+    changeMotorPositionTwo(dir);
+  }
+}
+
+void moveForward(int motorGroup) {
   if (motorGroup == 1) {
-    switch(step_number_1) {
-      case 0:
-        digitalWrite(SP1, HIGH);
-        digitalWrite(SP2, LOW);
-        digitalWrite(SP3, LOW);
-        digitalWrite(SP4, LOW);
-        changeMotorPositionOne();
-        break;
-      case 1:
-        digitalWrite(SP1, LOW);
-        digitalWrite(SP2, HIGH);
-        digitalWrite(SP3, LOW);
-        digitalWrite(SP4, LOW);
-        changeMotorPositionOne();
-        break;
-      case 2:
-        digitalWrite(SP1, LOW);
-        digitalWrite(SP2, LOW);
-        digitalWrite(SP3, HIGH);
-        digitalWrite(SP4, LOW); 
-        changeMotorPositionOne();
-        break;
-      case 3:
-        digitalWrite(SP1, LOW);
-        digitalWrite(SP2, LOW);
-        digitalWrite(SP3, LOW);
-        digitalWrite(SP4, HIGH);
-        changeMotorPositionOne();
-        break;
-    } 
+    //right
+    moveLeftMotor(false);
+    moveRightMotor(true);
+    //forward
   } else if (motorGroup == 3) {
-    switch(step_number_1) {
-      case 0:
-        digitalWrite(SP1, HIGH);
-        digitalWrite(SP2, LOW);
-        digitalWrite(SP3, LOW);
-        digitalWrite(SP4, LOW);
-        changeMotorPositionOne();
-        break;
-      case 1:
-        digitalWrite(SP1, LOW);
-        digitalWrite(SP2, HIGH);
-        digitalWrite(SP3, LOW);
-        digitalWrite(SP4, LOW);
-        changeMotorPositionOne();
-        break;
-      case 2:
-        digitalWrite(SP1, LOW);
-        digitalWrite(SP2, LOW);
-        digitalWrite(SP3, HIGH);
-        digitalWrite(SP4, LOW); 
-        changeMotorPositionOne();
-        break;
-      case 3:
-        digitalWrite(SP1, LOW);
-        digitalWrite(SP2, LOW);
-        digitalWrite(SP3, LOW);
-        digitalWrite(SP4, HIGH);
-        changeMotorPositionOne();
-        break;
-    } switch (step_number_2) {
-      case 0:
-        digitalWrite(SP5, HIGH);
-        digitalWrite(SP6, LOW);
-        digitalWrite(SP7, LOW);
-        digitalWrite(SP8, LOW);
-        changeMotorPositionTwo();
-        break;
-      case 1:
-        digitalWrite(SP5, LOW);
-        digitalWrite(SP6, HIGH);
-        digitalWrite(SP7, LOW);
-        digitalWrite(SP8, LOW);
-        changeMotorPositionTwo();
-        break;
-      case 2: 
-        digitalWrite(SP5, LOW);
-        digitalWrite(SP6, LOW);
-        digitalWrite(SP7, HIGH);
-        digitalWrite(SP8, LOW);
-        changeMotorPositionTwo();
-        break;
-      case 3:
-        digitalWrite(SP5, LOW);
-        digitalWrite(SP6, LOW);
-        digitalWrite(SP7, LOW);
-        digitalWrite(SP8, HIGH); 
-        changeMotorPositionTwo();
-        break;
-    }
+    moveLeftMotor(true);
+    moveRightMotor(true);
   } else if (motorGroup == 2) {
-    switch(step_number_2) {
-      case 0:
-        digitalWrite(SP5, HIGH);
-        digitalWrite(SP6, LOW);
-        digitalWrite(SP7, LOW);
-        digitalWrite(SP8, LOW);
-        changeMotorPositionTwo();
-        break;
-      case 1:
-        digitalWrite(SP5, LOW);
-        digitalWrite(SP6, HIGH);
-        digitalWrite(SP7, LOW);
-        digitalWrite(SP8, LOW);
-        changeMotorPositionTwo();
-        break;
-      case 2: 
-        digitalWrite(SP5, LOW);
-        digitalWrite(SP6, LOW);
-        digitalWrite(SP7, HIGH);
-        digitalWrite(SP8, LOW);
-        changeMotorPositionTwo();
-        break;
-      case 3:
-        digitalWrite(SP5, LOW);
-        digitalWrite(SP6, LOW);
-        digitalWrite(SP7, LOW);
-        digitalWrite(SP8, HIGH); 
-        changeMotorPositionTwo();
-        break;
-    }    
+    //left
+    moveLeftMotor(true);
+    moveRightMotor(false);
+  } else if (motorGroup == 4) {
+    moveLeftMotor(false);
+    moveRightMotor(false);
   }
 }
